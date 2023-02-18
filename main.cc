0b270983a1c98fcee7eb9c499630c3a07184c8f1
LemonSwank <kar@rrossinc.com>
2023-02-18 11:47:25 -0700

    main now generates an image, & progress bar.

--- main.cc
+++ main.cc
@@ -9,7 +9,8 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j = image_height; j>=0;--j){
-        for (int i=0; i>=image_width;++i){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i=0; i < image_width;++i){
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.25;
@@ -21,4 +22,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "\nDone.\n";
 }
