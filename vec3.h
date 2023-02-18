17bd0f21c11b1f0a9f343c5b47c78997bef91311
LemonSwank <kar@rrossinc.com>
2023-02-18 14:17:17 -0700

    compatibility fixes

--- vec3.h
+++ vec3.h
@@ -65,10 +65,8 @@ class vec3 {
 using point3 = vec3; // 3d Point
 using color = vec3;   // RGB Color
 
-#endif
-
 // vec3 Utility Functions
-inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
@@ -76,11 +74,11 @@ inline vec3 operator+(const vec3 &u, const vec3 &v){
     return vec3(u.e[0]+ v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3 &u, const vec3 &v) {
+inline vec3 operator-(const vec3 &u, const vec3 &v){
     return  vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &u, const vec3 &v) {
+inline vec3 operator*(const vec3 &u, const vec3 &v){
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
@@ -97,9 +95,9 @@ inline vec3 operator/(vec3 v, double t){
 }
 
 inline double dot(const vec3 &u, const vec3 &v) {
-    return u.e[0] * v.e[0]
-        +  u.e[1] * v.e[1]
-        +  u.e[2] * v.e[2];
+    return (u.e[0] * v.e[0]
+         +  u.e[1] * v.e[1]
+         +  u.e[2] * v.e[2]);
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v){
@@ -111,3 +109,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v){
 inline vec3 unit_vector(vec3 v){
     return v / v.length();
 }
+
+#endif
