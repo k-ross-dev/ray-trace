98719f8462a8e28b83febe3d9a06ee8c143a3fd3
LemonSwank <kar@rrossinc.com>
2023-02-18 12:34:58 -0700

    Class added for vector geometry.

--- main.cc
+++ main.cc
@@ -1,3 +1,11 @@
+//==============================================================================================
+// Written by kar-000.
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//==============================================================================================
+
 #include <iostream>
 
 int main() {
